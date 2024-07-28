a9eead8aaa5356b84cad4e983bba774fe1cd89a7
SumbulAziz <sumbulaziz100@gmail.com>
2024-07-29 01:08:46 +0530

    python qt2

---
+++ pythonqna2.py
@@ -0,0 +1,68 @@
+#q1)Create an empty dictionary
+dict={}
+print(dict)
+#q2)Create a dictionary with key-value pairs for a person's name and age
+dict={'rida': 20 , 'ayaz':55, "bilal":40}
+print(dict)
+#q3)Access the value associated with the key "age" in the person dictionary
+print(dict.values())
+#q4)Check if a key exists in the person dictionary.
+#q5)Add a new key-value pair "city" with the value "New York" to the person dictionary
+key='City'
+value="New york"
+dict[key]=value
+print(dict)
+#q6)Remove the key-value pair with the key "age" from the person dictionary
+dict.pop('ayaz')
+print(dict)
+#q7)Iterate through the keys in the person dictionary.
+for key in dict:
+    print(key)
+#q8)Iterate through the values in the person dictionary
+#q9)Iterate through both keys and values in the person dictionary.
+#q10)Create a dictionary with default values using the dict.from keys() method
+#q11)Get the value associated with a key using the dict.get() method with a default value.
+x=dict.get("musa",25)
+print(x)
+#q12)Count the number of occurrences of each character in a string using a dictionary.
+#q13)Merge two dictionaries.
+dict2={'a':10,'b':20,'c':30}
+print((dict|dict2))
+#q14)Create a dictionary where the values are squares of the keys
+sq={x: x**2 for x in range(1,8)}
+print(sq)
+#q15)Find the key with the maximum value in a dictionary.
+#q16)Sort a dictionary by its keys.
+sort_dict= (sorted (dict.keys()))
+print(sort_dict)
+#q17)Check if two dictionaries are equal.
+if dict == dict2:
+    print ("both are equal")
+else:
+    print ("both are not equal")
+#q18)Remove all elements from a dictionary.
+dict.clear()
+print(dict)
+#q19)Create a nested dictionary
+nesteddict = {1:{'rida': 20 , 'City':'New york', "bilal":40},2:{'a':10,'b':20,'c':30}}
+print(nesteddict[1]['rida'])
+print(nesteddict[2]['c'])
+print(nesteddict[1]['City'])
+#q20)Get a list of all keys in a dictionary
+lists=(dict2.keys())
+print(lists)
+#q21)Get a list of all values in a dictionary.
+lists1=(dict2.values())
+print(lists1)
+#q22)Update values in a dictionary using another dictionary
+dict.update(dict2)
+print(dict)
+#q23)Remove a key from a dictionary using the pop() method.
+dict.pop("a")
+print(dict)
+#q24)Get a list of tuples containing key-value pairs from a dictionary.
+list=[]
+list=(dict.items())
+print(list)
+#q25)Convert a list of tuples into a dictionary.
+
